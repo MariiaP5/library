7c054fc429810cbbcc3a591f75233e85f3a97ea5
MariiaP5 <138658021+MariiaP5@users.noreply.github.com>
2026-09-13 12:38:29 +0300

    Create book.py
    add class book

---
+++ book.py
@@ -0,0 +1,7 @@
+class Book:
+    def __init__(self, title, author, isbn, genre):
+        self.title = title
+        self.author = author
+        self.isbn = isbn
+        self.genre = genre
+        self.is_available = True
